bc3d4f44067ef4f446f14ff0309aae56720a8ea4
NicolasMachado <nicoma63@gmail.com>
2017-06-08 13:08:22 +0200

    challenge done

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 angular.module('myApp', [])
-  .controller('MyCtrl', function() {
+  .controller('MultiplicationCtrl', function($attrs) {
     var vm = this;
-    vm.numbers = [1,2,3,4];
+    vm.numberLimit = $attrs.initialNumberLimit || 10;
+    vm.numbers = function() {
+      var numbers = [];
+      for (var i=0; i<vm.numberLimit; i++) {
+        numbers[i] = i + 1;
+      };
+      return numbers;
+    }
+    vm.compute = function(a,b) {
+      return a * b;
+    };
   });
